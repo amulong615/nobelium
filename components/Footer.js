c026d2e5696bd174eb24412a8c58c97cd237e65f
A Mulong <124489360+amulong615@users.noreply.github.com>
2025-07-25 19:13:11 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 import { useConfig } from '@/lib/config'
-import Vercel from '@/components/Vercel'
+
 const Footer = ({ fullWidth }) => {
   const BLOG = useConfig()
-
   const d = new Date()
   const y = d.getFullYear()
   const from = +BLOG.since
+
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
@@ -14,11 +14,11 @@ const Footer = ({ fullWidth }) => {
     >
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
-        <div className="flex align-baseline justify-between flex-wrap">
+        <div className="flex align-baseline justify-center flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            © {from && from !== y ? `${from} - ` : ''}
+            {y} {BLOG.author} 的个人博客 版权所有 | 联系邮箱：i@your-email.com
           </p>
-          <Vercel />
         </div>
       </div>
     </div>
